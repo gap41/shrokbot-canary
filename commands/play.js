19a9b4346c222d4d71ad316f7de4d4221fa48e7c
MindChirp <frikk44@gmail.com>
2021-09-21 00:18:57 +0200

    initial commit

---
+++ commands/play.js
@@ -0,0 +1,11 @@
+const {prefix, token} = require('../config.json');
+
+module.exports = {
+    name: "play",
+    description: "Plays music from YouTube",
+    execute(message, args) {
+
+        message.channel.send("Penis");
+
+    }
+}
