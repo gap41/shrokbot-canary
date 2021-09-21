940f4c7626631846bc1da2d9a118036d6f2eafc8
gap41 <william@varholm.no>
2021-09-21 23:09:35 +0200

    Update package-lock.json

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const updateConfig = {
     executeOnComplete: path.join(__dirname, "restart.bat"),
     exitOnComplete: true
 }
-//et eller annet
+//et eller annet (v2)
 
 async function updateCheckLoop() {
     
